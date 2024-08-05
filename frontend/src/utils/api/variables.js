87f92eb69cbe7a3633512bd012cfd7846023170b
tripkm <pkmt28@gmail.com>
2024-08-05 14:36:06 +0700

    update(ip):change ip from public to private

--- frontend/src/utils/api/variables.js
+++ frontend/src/utils/api/variables.js
@@ -1,8 +1,8 @@
 export const variables = {
-    BASE_URL: "http://13.229.76.145:5214/api/",
-    USER_API: "http://13.229.76.145:5214/api/user",
-    PRODUCT_API: "http://13.229.76.145:5214/api/product",
-    ORDER_API: "http://13.229.76.145:5214/api/order",
-    PRODUCTSIZE_API: "http://13.229.76.145:5214/api/productsize",
-    ORDERITEM_API: "http://13.229.76.145:5214/api/orderitem",
+    BASE_URL: "http://172.31.35.33:5214/api/",
+    USER_API: "http://172.31.35.33:5214/api/user",
+    PRODUCT_API: "http://172.31.35.33:5214/api/product",
+    ORDER_API: "http://172.31.35.33:5214/api/order",
+    PRODUCTSIZE_API: "http://172.31.35.33:5214/api/productsize",
+    ORDERITEM_API: "http://172.31.35.33:5214/api/orderitem",
 }
